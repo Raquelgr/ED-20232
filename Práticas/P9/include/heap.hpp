2aa7417105748ff340bb1198bc4a794cae15b83e
raquel.mac <raquelrosa14@gmail.com>
2023-11-12 17:13:40 -0300

    P9

---
+++ Práticas/P9/include/heap.hpp
@@ -0,0 +1,27 @@
+#ifndef HEAP_HPP
+#define HEAP_HPP
+
+#include <unionFind.hpp>
+
+class Heap {
+
+    public:
+        Heap(int maxsize);
+        ~Heap();
+
+        void Inserir(Aresta x);
+        Aresta Remover();
+
+        bool Vazio();
+
+    private:
+        int GetAncestral(int posicao);
+        int GetSucessorEsq(int posicao);
+        int GetSucessorDir(int posicao);
+
+        int tamanho;
+        Aresta* data;
+};
+
+
+#endif
